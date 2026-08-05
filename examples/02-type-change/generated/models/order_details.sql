5a32eb34e8d74abce6e4e0a841679dea409c7821
ZEMBA Wendemi Brice Roméo <112257804+BriceZemba@users.noreply.github.com>
2026-08-05 04:33:34 +0000

    docs: re-freeze examples with the corrected layout

--- examples/02-type-change/generated/models/order_details.sql
+++ examples/02-type-change/generated/models/order_details.sql
@@ -4,7 +4,7 @@ select
     order_id,
     customer_id,
     order_date,
-    order_total::INT as order_total,
+    cast(order_total as double) as order_total,
     promotion_id,
     payment_method_code,
     cost_of_delivery
